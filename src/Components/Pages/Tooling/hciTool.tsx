a916b47b49007ba8f500cf4447aa6d20288bc658
rick-simons <71262565+rick-simons@users.noreply.github.com>
2022-01-20 13:06:13 +0100

    added blank tooling pages

---
+++ src/Components/Pages/Tooling/hciTool.tsx
@@ -0,0 +1,15 @@
+import {
+    Box,
+    Container,
+    Image,
+    Text,
+    Divider,
+    ListItem,
+    UnorderedList,
+    Link,
+  } from "@chakra-ui/react";
+  
+  export const HciTool = () => (
+    <></>
+    
+  )
